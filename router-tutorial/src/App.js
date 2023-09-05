1280a063b8068e2daf5ba3820685aa6f35aa3a4b
hoyoun <hoyoun1309@gmail.com>
2023-09-05 17:48:16 +0900

    5.3 서브라우트

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes, Link} from 'react-router-dom';
 import About from "./About";
 import Home from "./Home";
-import Profile from "./Profile";
+import Profiles from "./Profiles";
 
 const App = () => {
   return (
@@ -14,12 +14,15 @@ const App = () => {
         <li>
           <Link to="/about">소개</Link>
         </li>
+        <li>
+          <Link to="/profiles">프로필 목록</Link>
+        </li>
       </ul>
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile/>} />
+        <Route path="/profiles/*" element={<Profiles/>} />
       </Routes>
 
     </div>
