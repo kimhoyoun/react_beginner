b098d1749e637c5a5760abf82bdfb332727cd4c1
hoyoun <hoyoun1309@gmail.com>
2023-09-06 15:04:14 +0900

    6.6 개발자도구 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
 import rootReducer from './modules';
+import { composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
