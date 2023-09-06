c0d4a47f5015b2053bd27920ffe82816278697e9
hoyoun <hoyoun1309@gmail.com>
2023-09-06 16:31:55 +0900

    6.7 Todo List 구현

--- learn-redux/src/App.js
+++ learn-redux/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainers";
 
 function App() {
   return (
     <div>
       <CounterContainer />
+      <hr/>
+      <TodosContainer />
     </div>
   );
 }
